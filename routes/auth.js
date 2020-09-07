15434091408e323cb54079da8e7e1e18e72e70aa
Arindam <karmakararindam10@gmail.com>
2020-09-08 00:18:39 +0530

    Hashed Password

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const User = require ('../model/User');
 const {registerValidation} = require('../routes/validation');
-
+const bcrypt = require ('bcryptjs');
 
 router.post('/register', function (req,res){
 
@@ -10,21 +10,29 @@ router.post('/register', function (req,res){
     if (error) return res.status(400).send(error.details[0].message);
 
 // Checking if the User already exist in our database or not
-const emailExist =  User.findOne({email: req.body.email});
-if (emailExist) return res.status(400).send("Email already exists.");
-
-
-// Create a new user
- const user = new User({
-   name: req.body.name,
-   email: req.body.email,
-   password: req.body.password
- });
- user.save((err,savedUser)=>{
-   if (err)
-   return console.log(err)
-   return res.json(savedUser)
- })
+User.findOne({email: req.body.email}, function(err,emailExist){
+  if (err) {
+      return res.status(400).send("Email already exists.");
+}
+else {
+// Hashed password
+bcrypt.genSalt(10, function(err, salt) {
+  bcrypt.hash(req.body.password, salt, function(err, hashedPassword) {
+      // Store hash in your password DB.
+      // Create a new user
+      const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword
+      });
+      user.save((err,savedUser)=>{
+        if (err)
+        return console.log(err)
+        return res.json(savedUser)
+      });
+    });
+      });
+  }
 });
-
+});
 module.exports = router;
